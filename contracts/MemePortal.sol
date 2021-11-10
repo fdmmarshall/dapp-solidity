e08cd6f615ae37058a981d08350637b80f6ab00f
Flor Marshall <flordmarshall@gmail.com>
2021-11-09 21:24:36 -0500

    add logic for ipfs

--- contracts/MemePortal.sol
+++ contracts/MemePortal.sol
@@ -6,18 +6,27 @@ import "hardhat/console.sol";
 
 contract MemePortal {
     uint256 totalMemes;
+    string ipfsHash;
 
     constructor() {
         console.log("I love Tacos");
     }
 
+    function set(string memory x) public {
+        ipfsHash = x;
+    }
+
+    function get() public view returns (string memory) {
+        return ipfsHash;
+    }
+
     function sendMeme() public {
         totalMemes += 1;
-        console.log('%s has sent you a meme!', msg.sender);
+        console.log("%s has sent you a meme!", msg.sender);
     }
 
     function getTotalMemes() public view returns (uint256) {
-        console.log('We have %d total memes!', totalMemes);
+        console.log("We have %d total memes!", totalMemes);
         return totalMemes;
     }
 }
